48c061608d2e4b7e2c9189fa4f8ae817996ea460
Edward <ls2618332@gmail.com>
2026-04-06 19:10:39 +0300

    Mapper removed.

--- Program/Server/Application/Interfaces/IMapper.cs
+++
@@ -1,8 +0,0 @@
-namespace Application.Interfaces;
-
-public interface IMapper
-{
-    TDestination Map<TSource, TDestination>(TSource source);
-    IEnumerable<TDestination> MapList<TSource, TDestination>(IEnumerable<TSource> sources);
-    IQueryable<TDestination> ProjectTo<TSource, TDestination>(IQueryable<TSource> query);
-}
--- Program/Server/Application/Interfaces/IMapperTSourceTDestination.cs
+++
@@ -1,9 +0,0 @@
-namespace Application.Interfaces;
-
-public interface IMapper<TSource, TDestination>
-{
-    //Мэппер простой, один метод для конвертирования и кэширования конвертируемых типов
-    TDestination Map(TSource source);
-    IEnumerable<TDestination> MapList(IEnumerable<TSource> sources);
-    IQueryable<TDestination> ProjectTo(IQueryable<TSource> query);
-}
